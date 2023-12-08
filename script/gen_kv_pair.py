e014cea9b7050755196fb75b74fe4f5cffaef3df
Linjie Ma <malj@shanghaitech.edu.cn>
2023-12-09 01:41:03 +0800

    change generation of test case
    1. output unique key
    2. output res at the same time of outputing kv pair

--- script/gen_kv_pair.py
+++ script/gen_kv_pair.py
@@ -5,24 +5,40 @@ import random
 total_kv_num   =  100000
 unique_key_num =  20000
 
-unique_key_list = []
+unique_key_list = set()
 
-real_unique_key = set()
+key_store = set()
 
 random.seed(100)
 
 while(len(unique_key_list)!=unique_key_num):
     temp = random.randint(28495,234878620)
     if temp not in unique_key_list:
-        unique_key_list.append(temp)
+        unique_key_list.add(temp)
 
-with open("key_value_demo.txt","w") as f:
-    for i in range(total_kv_num):
-        temp = random.choices(unique_key_list)[0]
-        if temp not in real_unique_key:
-            real_unique_key.add(temp)
-        f.write(str(temp)+"\n")
+unique_key_list = list(unique_key_list)
 
+file_n = "test_case/key_value_100K"
+with open(file_n+".txt","w") as output_kv:
+    with open(file_n+"_res.txt","w") as output_res:
+        # iterate for total_kv_num times
+        for i in range(total_kv_num):
+            # ramdonly pick a key
+            temp = random.choices(unique_key_list)[0]
+            # write the key out to file
+            output_kv.write(str(temp)+"\n")
+            # if the key does not exist in the key_store, insert it, output not find
+            if temp not in key_store:
+                key_store.add(temp)
+                output_res.write("not find\n")
+            # else, output the key
+            else:
+                output_res.write(str(temp)+"\n")
+
+with open(file_n + "_key.txt","w") as f:
+    temp = list(key_store)
+    for i in temp:
+        f.write(str(i)+"\n")
 
 print("total kv pair number: "+str(total_kv_num))
-print("unique key number: "+str(len(real_unique_key)))
+print("unique key number: "+str(len(key_store)))
